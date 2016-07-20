f8a2fdc4f72e41bb94f18a0eb072fa5ac201cf53
Dan Kaiser <dskaiser82@gmail.com>
2016-07-19 20:13:59 -0700

    clean up code

--- server.js
+++ server.js
@@ -12,9 +12,6 @@ var results = [];
 
   function Yeah(){
       for (var i = 0; i < 6; i++) {
-      // console.log(i)
-      // console.log(phoneBook[i].firstName)
-      // console.log("starting next loop")
       if(searchTerm == phoneBook[i].lastName || searchTerm.includes(phoneBook[i].lastName)){
         results = "Name Found: "+phoneBook[i].lastName+", "+phoneBook[i].firstName+"."
         console.log(results)
@@ -26,30 +23,8 @@ var results = [];
   Yeah()
 
 
-//change data map to All jacobs, all Chans,
-
-
 // var potato = "Ivar Chan".includes("Cha");
 // console.log(potato)
-
-// The string:
-// var str = "Chan";
-//
-// // Look for "Hello"
-// var patt = phoneBook[1].lastName
-// var result = patt.test(str);
-//  console.log(patt)
-//
-// function Yolo(){
-// for (var i = 0; i < 6; i++) {
-//     if(result === true){
-//     returnNames = phoneBook[i].lastName+", "+phoneBook[i].firstName+"."
-//      console.log(returnNames)}
-//   }
-// }
-//
-// Yolo()
-
 
 
 
