984a4fd58d6949ff37d34694485eb91f355d5d33
Dan Kaiser <dskaiser82@gmail.com>
2016-07-19 18:48:06 -0700

    new phonebook

---
+++ model/map.js
@@ -0,0 +1,48 @@
+
+var phoneBook = {
+  // "Ivar Chan":{
+  //   phone:626+"-"+555+"-"+51234
+  // },
+  0:{
+    firstName:"Ivar",
+    lastName: "Chan",
+    phone:626+"-"+555+"-"+51234
+  },
+  1:{
+    firstName:"Dan",
+    lastName: "Kaiser",
+    phone:818+"-"+464+"-"+4746
+  },
+  2:{
+    firstName:"Arturo",
+    lastName: "Kaiser",
+    phone:905+"-"+555+"-"+1700
+  },
+  3:{
+    firstName:"Josh",
+    lastName: "Chan",
+    phone:818+"-"+464+"-"+1800
+  },
+  4:{
+    firstName:"John",
+    lastName: "Chan",
+    phone:818+"-"+464+"-"+1900
+  },
+  5:{
+    firstName:"Julie",
+    lastName: "Summer",
+    phone:818+"-"+464+"-"+2000
+  }
+} //end
+
+module.exports = phoneBook;
+
+
+//
+//
+// module.exports= {
+//
+// var phoneBook = 5
+//  //
+//
+// }
